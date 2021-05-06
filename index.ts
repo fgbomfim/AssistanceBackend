f38ac45a7a4e7d4ed77ef7d4f7821a4a574c98c2
Fernando G. Bomfim <f.gomes.bomfim@gmail.com>
2021-05-05 22:21:04 -0300

    Ajuste no dotenv

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
 import express from 'express';
-import { config } from 'dotenv';
 import { json } from 'body-parser';
 import cors from 'cors';
 import { router } from './src/router';
-
-config();
 
 const PORT = 3000;
 const app = express();
--- knexfile.ts
+++ knexfile.ts
@@ -1,3 +1,7 @@
+import {config} from 'dotenv';
+
+config();
+
 const knexfile = {
   client: process.env.DB_CLIENT,
   connection: {
