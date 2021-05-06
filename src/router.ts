7b28ab890e625a58eadc63a22adfbc102ab5745c
Fernando G. Bomfim <f.gomes.bomfim@gmail.com>
2021-05-06 20:15:52 -0300

    New route

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { request, Router } from "express";
 import { createUserController } from "./useCases/CreateUser";
 import { loginController } from "./useCases/Login";
+import { createAssistanceController } from "./useCases/CreateAssistance"
 
 const router = Router();
 
@@ -10,6 +11,9 @@ router
   })
   .post('/oauth2/vi/signup', (request, response) => {
     return loginController.handle(request, response);
+  })
+  .post('/assistance', (request, response) => {
+    return createAssistanceController.handle(request, response);
   });
 
 export { router }
